9ff63e80b81c5e5855c269b83bed00cdd665c555
DaysonTai1848 <daysontai69@gmail.com>
2024-04-18 19:43:20 +0800

    first commit

---
+++ index.ts
@@ -0,0 +1,17 @@
+import { Hono } from "hono";
+import { todos } from "./data.json";
+
+const app = new Hono();
+
+// c = context
+app.get("/todos/:id", (c) => {
+  // todoId is a string
+  const todoId = c.req.param("id");
+  // convert todoId to number
+  const todoIndex = Number(todoId);
+  // get todo from todos array
+  const todo = todos[todoIndex - 1] || {};
+  return c.json({ todo });
+});
+
+export default app;
